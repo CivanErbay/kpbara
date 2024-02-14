b09e9b5b747519b155cb0feee167220e1d79fbca
Civan Erbay <info@civan-erbay.de>
2024-02-14 23:19:46 +0100

    Setup

---
+++ components/Hero.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="mx-4 relative">
+    <img src="../assets/images/Rectangle_4.svg" alt="">
+    <img class="absolute top-0 left-0 h-3/4" src="../assets/images/Rectangle_5.png" alt="">
+    <img class="absolute top-0 right-20 h-40" src="../assets/images/Rectangle_8.png" alt="">
+    <div>
+      <div></div>
+      <div class="font-inter">Blub</div>
+    </div>
+
+  </div>
+</template>
+
+<script setup>
+
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <Navbar></Navbar>
+  <Hero></Hero>
+</template>
+
+<script setup></script>
